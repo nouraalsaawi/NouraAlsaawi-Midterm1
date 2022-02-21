5c76625bac94ab03e39d79c3e73e791e544bf184
nouraalsaawi <99714614+nouraalsaawi@users.noreply.github.com>
2022-02-21 12:51:27 +0300

    first

--- app/src/main/java/com/example/se328midterm1/MainActivity3.java
+++ app/src/main/java/com/example/se328midterm1/MainActivity3.java
@@ -1,19 +1,25 @@
 package com.example.se328midterm1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ListAdapter;
 
+import android.app.ListActivity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
-public class MainActivity3 extends AppCompatActivity {
-
+public class MainActivity3 extends ListActivity {
+    String[] i={"First","Second","Third","Fourth "};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
         final Button act2main= (Button) findViewById(R.id.act21b);
+        setListAdapter(new ArrayAdapter<String>(this,R.layout.activity_main3,R.id.list,i));
 
         act2main.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,4 +29,27 @@ public class MainActivity3 extends AppCompatActivity {
             }
         });
     }
+
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+
+        super.onListItemClick(l, v, position, id);
+
+        switch(position){
+            case 0:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.alfaisal.edu")));
+                break;
+            case 1:
+                startActivity(new Intent(MainActivity3.this, picture.class));;
+                break;
+            case 2:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.alfaisal.edu")));
+                break;
+            case 3:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.alfaisal.edu")));
+                break;
+        }
+
+
+    }
 }
+
